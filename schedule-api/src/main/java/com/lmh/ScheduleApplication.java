aecb03ada5c2536eb7ce5a91ba4e51ab401bdf77
minho2618 <minho2618@naver.com>
2026-05-18 13:27:03 +0900

    Add - BaseTimeEntity 추가

--- schedule-api/src/main/java/com/lmh/ScheduleApplication.java
+++ schedule-api/src/main/java/com/lmh/ScheduleApplication.java
@@ -2,8 +2,10 @@ package com.lmh;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing // 이 어노테이션이 없으면 createdAt이 null이 됨
 public class ScheduleApplication {
 
 	public static void main(String[] args) {
